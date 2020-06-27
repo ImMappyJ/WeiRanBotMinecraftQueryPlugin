79eead3d072cf87c789fa54c3dfdbb4f7deadbfa
MappyJ <1603931150@qq.com>
2020-06-27 17:41:21 +0800

    add levelcalculation

--- src/main/java/net/hypixel/api/reply/GuildReply.java
+++ src/main/java/net/hypixel/api/reply/GuildReply.java
@@ -3,8 +3,7 @@ package net.hypixel.api.reply;
 import net.hypixel.api.util.Banner;
 
 import java.time.ZonedDateTime;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 public class GuildReply extends AbstractReply {
     private Guild guild;
@@ -35,6 +34,36 @@ public class GuildReply extends AbstractReply {
         private Boolean joinable;
         private long exp;
         private int legacyRanking;
+
+        public int getLevel(){
+            List<Integer> needLevelList = Arrays.asList(
+                    100000, // Lvl 0 -> Lvl 1
+                    150000, // Lvl 1 -> Lvl 2
+                    250000, // Lvl 2 -> Lvl 3
+                    500000, // Etc
+                    750000,
+                    1000000,
+                    1250000,
+                    1500000,
+                    2000000,
+                    2500000,
+                    2500000,
+                    2500000,
+                    2500000,
+                    2500000,
+                    3000000
+            );
+            long exp = this.exp;
+            int maxNeedLevel = needLevelList.get(needLevelList.size()-1);
+            for(int level = 0;;level++){
+                if(level>=14){
+                    exp-=maxNeedLevel;
+                }else{
+                    exp-=needLevelList.get(level);
+                }
+                if(exp<0) return level;
+            }
+        }
 
         public String get_id() {
             return _id;
